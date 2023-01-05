d76d4073219c116fcdbdc2ed0eb0d5e994e634fe
Shashi Raj <shashiraj.972@gmail.com>
2023-01-05 12:31:04 +0530

    Update the comments

--- Advanced-Java-J2EE-Notes/JDBC_Notes/src/com/bittercode/daytwo/B_RegisterCustomer_Assignment.java
+++ Advanced-Java-J2EE-Notes/JDBC_Notes/src/com/bittercode/daytwo/B_RegisterCustomer_Assignment.java
@@ -32,7 +32,11 @@ public class B_RegisterCustomer_Assignment {
 
 			// Create a model and use it here
 			Customer customer = new Customer();
+
+			// Create a unique id automatically and set to the customer model
 			customer.setId(System.currentTimeMillis() + "");
+
+			// Scan all the required field and set into the customer model to be used later
 			System.out.println("Enter Your Name");
 			customer.setName(scan.nextLine());
 			System.out.println("Enter Your Email");
@@ -48,7 +52,10 @@ public class B_RegisterCustomer_Assignment {
 			System.out.println("Enter Your Password");
 			customer.setPwd(scan.nextLine());
 
+			// Prepare the statement to insert into db
 			PreparedStatement ps = connection.prepareStatement("insert into customer values(?,?,?,?,?,?,?)");
+
+			// bind all the required values into the ps
 			ps.setString(1, customer.getId());
 			ps.setString(2, customer.getName());
 			ps.setString(3, customer.getEmail());
@@ -57,7 +64,11 @@ public class B_RegisterCustomer_Assignment {
 			ps.setString(6, customer.getDob());
 			ps.setString(7, customer.getPwd());
 			System.out.println(customer);
+
+			// Execute the query using correct method
 			int response = ps.executeUpdate();
+
+			// Return the response based on success
 			if (response > 0) {
 				System.out.println("Thank you for your registration, you have been successfully registered with id: "
 						+ customer.getId());
@@ -66,10 +77,9 @@ public class B_RegisterCustomer_Assignment {
 			}
 
 		} catch (SQLException e) {
-			System.out.println("Error while execution");
-			e.printStackTrace();
+			System.out.println("Error while execution" + e.getMessage());
 		} finally {
-			System.out.println("Program Executed!!");
+			System.out.println("Thanks for Visiting Us!!");
 		}
 
 	}
--- Advanced-Java-J2EE-Notes/JDBC_Notes/src/com/bittercode/daytwo/C_LoginCustomer_Assignment.java
+++ Advanced-Java-J2EE-Notes/JDBC_Notes/src/com/bittercode/daytwo/C_LoginCustomer_Assignment.java
@@ -31,19 +31,28 @@ public class C_LoginCustomer_Assignment {
 		try (Scanner scan = new Scanner(System.in);
 				Connection connection = DriverManager.getConnection(oracleConnectionString, username, password);) {
 
+			//Scan the email and password
 			Customer customer = new Customer();
 			System.out.println("Enter Your Email");
 			customer.setEmail(scan.nextLine());
 			System.out.println("Enter Your Password");
 			customer.setPwd(scan.nextLine());
 
+			//Create the query
 			PreparedStatement ps = connection
 					.prepareStatement("select * from customer where email = ? and password = ?");
+			
+			//bind the values
 			ps.setString(1, customer.getEmail());
 			ps.setString(2, customer.getPwd());
+			
+			//ExecuteQuery and store the response
 			ResultSet rs = ps.executeQuery();
+			
+			//If result set have some response that means login is success
 			if (rs.next()) {
 
+				//Read all the customer details from the result set
 				customer.setId(rs.getString("ID"));
 				customer.setDob(rs.getString("dob"));
 				customer.setGender(rs.getString("gender").charAt(0));
@@ -51,14 +60,14 @@ public class C_LoginCustomer_Assignment {
 				customer.setName(rs.getString("name"));
 
 				System.out.println("User Login Success!, Welcome to Bitter Code");
+				//Print the customer details
 				System.out.println(customer);
 			} else {
 				System.out.println("Invalid login credentials,try again!!");
 			}
 
 		} catch (SQLException e) {
-			System.out.println("Error while execution");
-			e.printStackTrace();
+			System.out.println("Error while execution" + e.getMessage());
 		}
 
 	}
